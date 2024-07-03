662468c0030e8e912aa8e6fc4599691f96d69f18
vikkoooo <vikkolundberg@gmail.com>
2024-07-03 15:42:16 +0200

    divide by zero block only if b = 0 not a = 0. more user messages

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Calculator Script 
+
+// Welcome message
+alert("Welcome to the calculator. Enter two numbers that you want to work with, and finish with the operation to execute and I will calculate that for you");
+
 // Prompt calls and variables
-const firstNum = getInputNumber("Enter first number"); // get the number from user
-const secondNum = getInputNumber("Enter second number");
-const operation = getInputOperation("Enter the math operation (only +, -, / or *)");
+const firstNum = getInputNumber("Enter the first number"); // get the number from user
+const secondNum = getInputNumber("Enter the second number");
+const operation = getInputOperation("Enter the math operation (only use '+', '-', '*' or '/')");
 const sum = calculate(firstNum, secondNum, operation);
 if (sum != null) { alert(`Result of ${firstNum} ${operation} ${secondNum} = ${sum}`); };
+
+// Exit script
+alert("Thank you for using the calculator. Refresh the website to try again");
 
 // User must enter input which is parsable to a number
 function getInputNumber(message) {
@@ -15,8 +22,9 @@ function getInputNumber(message) {
 		input = prompt(message);
 
 		// Check if it's parsable (make sure to trim and check for "" because some blankspace makes weird things happen)
-		if (input == null || input.trim() === "") { // https://stackoverflow.com/a/10232443
-			console.log(`Input was "${input}". Returning to start of loop.`);
+		if (input === null || input.trim() === "") { // https://stackoverflow.com/a/10232443
+			alert("Input can not be empty. Try again");
+			console.log(`Input was "${input}". Returning to start of loop`);
 			continue;
 		}
 
@@ -25,7 +33,8 @@ function getInputNumber(message) {
 
 		// if non parsable we will get NaN
 		if (isNaN(number)) {
-			console.log(`Input was "${input}". Returning to start of loop.`);
+			alert("Unaccepted input, please write numbers only. Try again");
+			console.log(`Input was "${input}". Returning to start of loop`);
 			continue;
 		} else {
 			// good input by now
@@ -38,7 +47,7 @@ function getInputNumber(message) {
 // User must enter operation input
 function getInputOperation(message) {
 	let operation;
-	const allowedOperations = ["+", "-", "/", "*"];
+	const allowedOperations = ["+", "-", "*", "/"];
 
 	while (true) {
 		operation = prompt(message);
@@ -48,7 +57,8 @@ function getInputOperation(message) {
 			console.log(`Returning input: ${operation} as string`);
 			return operation;
 		} else {
-			console.log(`Input was "${operation}". Returning to start of loop.`);
+			alert("Invalid operation input. Please enter only '+', '-', '*' or '/'. Try again");
+			console.log(`Input was "${operation}". Returning to start of loop`);
 		}
 	}
 }
@@ -62,14 +72,16 @@ function calculate(a, b, operation) {
 	} else if (operation === "*") {
 		return a * b;
 	} else if (operation === "/") {
-		if (a || b === 0) {
+		if (b === 0) {
 			alert(`Impossible to divide by 0!`);
+			console.log(`Error, user tried to divide ${a} by ${b}. Returning null`);
 			return null;
 		} else {
 			return a / b;
 		}
 	} else {
 		alert(`Some kind of unknown state has occured. Error!`);
+		console.log(`Error, some kind of unknown error has occured. a=${a}, b=${b}, operation=${operation}. Returning null`);
 		return null;
 	}
 }
